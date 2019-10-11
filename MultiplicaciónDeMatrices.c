ab00141046f41284cc98f50ca2cd0eec5e4874fe
Bernardo Bernal <bernardobernal@outlook.es>
2019-10-11 10:19:40 -0500

    Optimizacion

--- MultiplicaciónDeMatrices.c
+++ MultiplicaciónDeMatrices.c
@@ -1,3 +1,19 @@
+/*
+
+-Materia: Supercómputo
+-Semestre: 2019-2020/I
+-Nombre del alumno: Bernal Cabrera José Bernardo    
+-Clave del alumno: 248062
+-Carrera: Ing. Informática
+-Nombre del alumno: Méndez Gutiérrez    
+-Clave del alumno: 250980
+-Carrera: Ing. Informática
+-Nombre de tarea o programa: MPI: Funciones Gather y Scatter
+-Periodo de evaluación (parcial 1, 2, 3, 4 o 5): 2
+-Descripción: Programa en MPI que muestra el funcionamiento de estas funciones
+-Avance logrado (0 a 100%): 100%
+-Comentarios adicionales: Ninguno
+*/
 #include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
@@ -5,121 +21,112 @@
 #include "mpi.h"
 
 int getNumeroRandom();
-int *generaMatriz(int tam);
+int *creaArreglo(int tam);
 void llenaMatriz(int *matriz, int fil, int col);
 void imprime(int *matriz, int fil, int col);
-int validaArgumentos(int argc, char *argv[], int idProc);
+int validaArgumentos(int argc, char *argv[], int idProc, int *datos);
 int filaXColumna(int *fil, int *col, int datos);
 int *llenaFila(int *A, int tam, int indice);
 int *llenaColumna(int *A, int tam, int indice, int col);
+int *getArrayDeProcesos(int numProc, int nDatos, int nDatosU);
+int *getArrayDesplazamiento(int numProc, int nDatos);
 
 int main(int argc, char *argv[])
 {
     srand(time(NULL));
     setlocale(LC_ALL, "es_MX");
-    int idProc, numProc, nDatos, nDatosU, filA, colA, filB, colB;
+    int idProc, numProc, nDatos, nDatosU;
+    int *CBuf, *procesos, *direccionamiento, *A, *B, *C;
+    int datos[4]; // Indices 0: #Fil A 1: #Col A 2: #Fil B, 3: #Col B
     MPI_Init(&argc, &argv);
     MPI_Comm_rank(MPI_COMM_WORLD, &idProc);
     MPI_Comm_size(MPI_COMM_WORLD, &numProc);
     MPI_Status estado;
-    if (!validaArgumentos(argc, argv, idProc))
+    if (!validaArgumentos(argc, argv, idProc, datos))
     {
         MPI_Finalize();
         return 0;
     }
-    filA = atoi(argv[1]);
-    colA = atoi(argv[2]);
-    filB = atoi(argv[3]);
-    colB = atoi(argv[4]);
-    int *A = generaMatriz(filA * colA);
-    int *B = generaMatriz(filB * colB);
-    nDatos = (filA * colB) / (numProc - 1);
-    nDatosU = nDatos + (filA * colB) % (numProc - 1);
+    A = creaArreglo(datos[0] * datos[1]);
+    B = creaArreglo(datos[2] * datos[3]);
+    nDatos = (datos[0] * datos[3]) / (numProc);
+    nDatosU = nDatos + (datos[0] * datos[3]) % (numProc);
+    procesos = getArrayDeProcesos(numProc, nDatos, nDatosU);
+    direccionamiento = getArrayDesplazamiento(numProc, nDatos);
+    CBuf = creaArreglo(nDatos);
     if (idProc == 0)
     {
-        llenaMatriz(A, filA, colA);
-        llenaMatriz(B, filB, colB);
-        imprime(A, filA, colA);
-        imprime(B, filB, colB);
+        llenaMatriz(A, datos[0], datos[1]);
+        llenaMatriz(B, datos[2], datos[3]);
+        imprime(A, datos[0], datos[1]);
+        imprime(B, datos[2], datos[3]);
     }
-    MPI_Bcast(A, filA * colA, MPI_INT, 0, MPI_COMM_WORLD);
-    MPI_Bcast(B, filB * colB, MPI_INT, 0, MPI_COMM_WORLD);
+    MPI_Bcast(A, datos[0] * datos[1], MPI_INT, 0, MPI_COMM_WORLD);
+    MPI_Bcast(B, datos[2] * datos[3], MPI_INT, 0, MPI_COMM_WORLD);
     if (idProc == 0)
     {
-        int i, j, *parcial, cont;
-        cont = 0;
-        int *C = generaMatriz(filA * colB); //Matriz resultante
-        for (i = 1; i < numProc - 1; i++)
-        {
-            parcial = generaMatriz(nDatos);
-            MPI_Recv(parcial, nDatos, MPI_INT, i, 0, MPI_COMM_WORLD, &estado);
-            for (j = 0; j < nDatos; j++)
-            {
-                *(C + cont) = *(parcial + j);
-                cont++;
-            }
-        }
-        parcial = generaMatriz(nDatosU);
-        MPI_Recv(parcial, nDatosU, MPI_INT, i, 0, MPI_COMM_WORLD, &estado);
-        for (j = 0; j < nDatosU; j++)
-        {
-            *(C + cont) = *(parcial + j);
-            cont++;
-        }
-        imprime(C, filA, colB);
-        free(C);
+        C = creaArreglo(datos[0] * datos[3]); //Matriz resultante
     }
-    else
+    int *fil, *col, i, j, k;
+    fil = creaArreglo(datos[0]);
+    col = creaArreglo(datos[3]);
+    i = (idProc * nDatos) / datos[3];
+    j = (idProc * nDatos) % datos[3];
+    if(idProc == numProc - 1){
+        nDatos = nDatosU;
+    }
+    for (k = 0; k < nDatos; k++)
     {
-        int *fil, *col, *resultado, i, j, cont, n;
-        fil = generaMatriz(filA);
-        col = generaMatriz(colB);
-        cont = n = 0;
-        if (idProc != numProc - 1)
+        fil = llenaFila(A, datos[1], i);
+        col = llenaColumna(B, datos[2], j, datos[3]);
+        CBuf[k] = filaXColumna(fil, col, datos[0]);
+        if (j + 1 < datos[3])
         {
-            resultado = generaMatriz(nDatos);
+            j++;
+        }else{
+            j = 0;
+            i++;
         }
-        else
-        {
-            resultado = generaMatriz(nDatosU);
-        }
-        for (i = 0; i < filA; i++)
-        {
-            for (j = 0; j < colB; j++)
-            {
-                if (cont >= nDatos * (idProc - 1))
-                {
-                    if (n < nDatos)
-                    {
-                        fil = llenaFila(A, colA, i);
-                        col = llenaColumna(B, filB, j, colB);
-                        resultado[n] = filaXColumna(fil, col, filA);
-                        n++;
-                    }
-                    else{
-                        i = filA;
-                        break;
-                    }
-                }
-                cont++;
-            }
-        }
-        if (idProc == numProc - 1)
-        {
-            nDatos = nDatosU;
-        }
-        MPI_Send(resultado, nDatos, MPI_INT, 0, 0, MPI_COMM_WORLD);
     }
+    MPI_Gatherv(CBuf, nDatos, MPI_INT, C, procesos, direccionamiento, MPI_INT, 0, MPI_COMM_WORLD);
     free(A);
     free(B);
+    if (idProc == 0)
+    {
+        imprime(C, datos[0], datos[3]);
+        free(C);
+    }
     MPI_Finalize();
     return 0;
+}
+
+int *getArrayDeProcesos(int numProc, int nDatos, int nDatosU)
+{
+    int *procesos = creaArreglo(numProc);
+    int i;
+    for (i = 0; i < numProc - 1; i++)
+    {
+        *(procesos + i) = nDatos;
+    }
+    *(procesos + i) = nDatosU;
+    return procesos;
+}
+
+int *getArrayDesplazamiento(int numProc, int nDatos)
+{
+    int *desplazamiento = creaArreglo(numProc);
+    int i;
+    for (i = 0; i < numProc; i++)
+    {
+        *(desplazamiento + i) = i * nDatos;
+    }
+    return desplazamiento;
 }
 
 int *llenaFila(int *A, int tam, int indice)
 {
     int i, *fila;
-    fila = generaMatriz(tam);
+    fila = creaArreglo(tam);
     for (i = 0; i < tam; i++)
     {
         *(fila + i) = *(A + (indice * tam) + i);
@@ -131,7 +138,7 @@ int *llenaFila(int *A, int tam, int indice)
 int *llenaColumna(int *A, int tam, int indice, int col)
 {
     int i, *columna;
-    columna = generaMatriz(tam);
+    columna = creaArreglo(tam);
     for (i = 0; i < tam; i++)
     {
         *(columna + i) = *(A + indice + (i * col));
@@ -139,7 +146,7 @@ int *llenaColumna(int *A, int tam, int indice, int col)
     return columna;
 }
 
-int validaArgumentos(int argc, char *argv[], int idProc)
+int validaArgumentos(int argc, char *argv[], int idProc, int *datos)
 {
     if (argc < 5)
     {
@@ -189,6 +196,11 @@ int validaArgumentos(int argc, char *argv[], int idProc)
         }
         return 0;
     }
+    int i = 0;
+    for (; i < 4; i++)
+    {
+        datos[i] = atoi(argv[i + 1]);
+    }
     return 1;
 }
 
@@ -198,7 +210,7 @@ int getNumeroRandom(int limInf, int limSup)
     return rand() % limSup + limInf;
 }
 
-int *generaMatriz(int tam)
+int *creaArreglo(int tam)
 {
     int *matriz = (int *)malloc(tam * sizeof(int));
     return matriz;
